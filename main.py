815222e9a700f983f5eda83c9b91489a8986c9bd
grumpycatyo-collab <linkolnmax24@gmail.com>
2025-05-15 15:11:00 +0300

    Update port

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ logger.info(f"Application {settings.app_name} initialized")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
