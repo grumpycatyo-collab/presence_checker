3ad4363f1bb644472d859cb77b3fd9cd68670b8f
grumpycatyo-collab <linkolnmax24@gmail.com>
2025-05-15 23:51:29 +0300

    Add listener thread

---
+++ core/listener.py
@@ -0,0 +1,30 @@
+import serial
+import threading
+from .logger import logger
+
+def serial_listener(port='/dev/tty.usbserial-0001', baudrate=115200):
+    ser = serial.Serial(port, baudrate)
+    logger.info(f"Serial listener started on {port}")
+
+    while True:
+        try:
+            if ser.in_waiting:
+                data = ser.readline().decode().strip()
+                logger.info(f"Received: {data}")
+                parts = data.split(',')
+
+                if len(parts) == 4:
+                    uid, room, time_now, day = parts
+                    # message = attendance_crud.check_attendance(uid, room, time_now, day) # Should be implemented in the crud module
+                    message = "Good"
+                    logger.info("Sending:", message)
+                    ser.write((message + "\n").encode())
+        except Exception as e:
+            logger.error(f"Error in listener: {e}")
+            time.sleep(1)
+
+def start_listener_thread():
+    thread = threading.Thread(target=serial_listener, daemon=True)
+    thread.start()
+    return thread
+
