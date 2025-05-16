513fff66c0ad8e77ea48a8146184ace5ba1f6697
grumpycatyo-collab <linkolnmax24@gmail.com>
2025-05-16 11:28:12 +0300

    Add if statement for listener.py check

--- main.py
+++ main.py
@@ -28,9 +28,10 @@ async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
     logger.info("Database tables created")
 
-    from core.listener import start_listener_thread # TODO: Not tested
-    start_listener_thread()
-    logger.info("Serial listener started")
+    from core.listener import start_listener_thread
+    if settings.esp_32_connection != "wifi":
+        start_listener_thread()
+        logger.info("Serial listener started")
 
     yield
     # Shutdown
